d18ce4a166277d3c57a0f8c76d9841fb8d5210aa
gvaduha <gvaduha@gmail.com>
2019-05-29 18:48:53 +0700

    Execution framework completed

--- Program.fs
+++ Program.fs
@@ -22,13 +22,72 @@ let createBoard board initials =
                                                         | _ -> e::b))) (1,[])
     newBoard
 
+(* board raw *)
+let getRaw size board n =
+    board |> List.skip (size*n) |> List.take size
+
+(* board column *)
+let getCol size board n =
+    let (_, col) = [0..size-1]
+                   |> List.fold (fun (sl, dl) e -> let e = sl |> List.skip n |> List.take 1
+                                                   (sl |> List.skip size, e::dl))
+                                                   (board,[])
+    col |> List.rev
+
+
 (* prints board (ad-hoc)*)
 let printBoard size board =
-    board |> List.fold (fun i x ->  match (fst x) with
-                                        | None -> printf " |"
-                                        | v -> printf "%d|" ((fun (Value x) -> x) v)
-                                    if i % size = 0 then printfn ""
-                                    i+1) (1)
+    let _ = board |> List.fold (fun i x ->  match (fst x) with
+                                            | None -> printf " |"
+                                            | v -> printf "%d|" ((fun (Value x) -> x) v)
+                                            if i % size = 0 then printfn ""
+                                            i+1) (1)
+    printfn ""
+
+(**)
+let rawScan size board =
+    let b = board
+    let s = size
+    b
+
+(**)
+let colScan size board =
+    let b = board
+    let s = size
+    b
+
+(**)
+let boxScan size board =
+    let b = board
+    let s = size
+    b
+
+(**)
+let reduce size board =
+    let b = board
+    let s = size
+    b
+
+(**)
+let isIncompleted board =
+    let res = List.tryFind (fun x -> if fst x = None then true else false) board
+    match res with
+    | Some x -> true
+    | _ -> false
+
+(**)
+let rec simpleREPL size board =
+    let b = board |> rawScan size |>  colScan size |> boxScan size |> reduce size
+
+    printBoard size b
+
+    match (isIncompleted b) with
+    | false -> b
+    | true -> simpleREPL size b
+
+(**)
+let parallelREPL =
+    ()
 
 (* coordinates a values of known numbers ((x,y),val) *)
 let gameInitials = [
@@ -47,9 +106,12 @@ let gameInitials = [
 let main argv =
     let boardSize = 9
     let emptyBoard = getEmptyBoard boardSize
-    //printf "%A" (emptyBoard)
     let board = createBoard emptyBoard (initialValues boardSize gameInitials)
+    printBoard boardSize board
+    //printfn "%A" (getRaw boardSize board 0)
+    //printfn "%A" (getCol boardSize board 0)
+    //printfn "%A" (isIncompleted boardSize board)
 
-    let p = printBoard boardSize
-    p board
+    simpleREPL boardSize board
+    printBoard boardSize board
     0 // return an integer exit code
